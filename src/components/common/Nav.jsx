67ef82ff31f66cdac175ee9427d4fce99de96efb
AyaJafar <aya.jafar002@gmail.com>
2025-01-06 12:31:06 +0300

    Fix images path

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -6,7 +6,7 @@ export const Nav = () => {
       className="h-[7vh] flex items-center gap-2 px-10 py-10 bg-[#F3F6F8]"
       dir="rtl"
     >
-      <img src="public/logo.png" alt="logo" className="h-10" />
+      <img src="assets/images/logo.png" alt="logo" className="h-10" />
       <Typography variant="h5" className="font-heading font-meduim">
         العنوان
       </Typography>
